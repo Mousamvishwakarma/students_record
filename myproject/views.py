7c1fc855421fd3459635e96836b6fdbc07d9d139
mousam_official07 <mousamvishwakarma929@gmail.com>
2025-02-16 17:17:38 +0530

    Update views.py

--- myproject/views.py
+++ myproject/views.py
@@ -20,15 +20,3 @@ def indexpage(request):
         temp = studentdata (name = firstname, email = email, address = address )
         temp.save()
     return render(request,'index.html')
-
-#def marsk(request):
-#    if request.method =="POST":
-#        sub1 = request.POST.get('sub1')
-#        sub2 = request.POST.get('sub2')
-#        sub3 = request.POST.get('sub3')
-#        sub4 = request.POST.get('sub4')
-#        sub5 = request.POST.get('sub5')
-#
-#        temp = Studentmarks (sub1 = sub1, sub2 = sub2, sub3 = sub3, sub4 = sub4, sub5 = sub5)
-#        temp.save()
-#    return render(request,'marks.html')
